66d1b0fabee1ff3086877f56387fc5a3b2382305
cjasoni <cjasoni@gmail.com>
2014-10-31 11:41:41 +0800

    update package

--- src/com/cjasoni/cordova/plugins/DeviceEmailAccount.java
+++ src/com/cjasoni/cordova/plugins/DeviceEmailAccount.java
@@ -1,10 +1,11 @@
 package com.cjasoni.cordova.plugins;
-
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.PluginResult;
 
 import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import android.content.Context;
 import android.accounts.Account;
@@ -12,24 +13,34 @@ import android.accounts.AccountManager;
 
 public class DeviceEmailAccount extends CordovaPlugin {
 
- 
-    private String getAccount(AccountManager am) {
-        String str = "";
 
+   
+    private  JSONObject getAccounts(AccountManager am) {
+        String str = "";
+        JSONObject json = new JSONObject();
+        
+        
         if (am != null) {
             Account[] accounts = am.getAccounts();
 
             for (int i = 0; i < accounts.length; i++) {
-                if (str.length() > 0) {
-                    str += ",";
+                
+                
+                try {
+                    //if(checkValue(accounts[i].type) == "com.google"){
+                        json.put(String.valueOf(i),accounts[i].name);
+                    //}
+                        
+                } catch (JSONException e) {
+                        // TODO Auto-generated catch block
+                        //e.printStackTrace();
                 }
-
-                str += "account" + i + "Name: " + accounts[i].nam) + ","
-                        + "account" + i + "Type: " + accounts[i].type;
+                
+                
             }
         }
 
-        return str;
+        return json;
     }
 
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) {
@@ -37,9 +48,10 @@ public class DeviceEmailAccount extends CordovaPlugin {
             if (action.equals("get")) {
                 AccountManager am = AccountManager.get(this.cordova.getActivity());
 
-                String result = getAccount(am);
+                JSONObject result = getAccounts(am);
                 if (result != null) {
-                    callbackContext.success(result);
+                	callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.OK, result));
+                    //callbackContext.success(am);
                     return true;
                 }
             }
